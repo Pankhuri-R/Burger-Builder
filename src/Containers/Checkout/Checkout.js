ffb762b49f869e371ceb634eb2bae4c811760b9c
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-07-21 22:05:49 +0530

    changes

--- src/Containers/Checkout/Checkout.js
+++ src/Containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../Components/Order/CheckoutSummary/CheckoutSummary';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 
 import { connect } from 'react-redux';
@@ -35,16 +35,25 @@ class Checkout extends Component{
     }
 
     render(){
+        let summary = <Redirect to="/" />
+        if (this.props.ings) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ings}
+                        cancelCheckout={this.checkoutCancelHandler}
+                        continueCheckout={this.checkoutContdHandler} />
+                    <Route path={this.props.match.path + '/contact-data'}
+                        //  render= {(props)=>(<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)}
+                        component={ContactData}
+                    />
+                </div>
+            );
+        }
         return (
             <div>
-                <CheckoutSummary 
-                ingredients={this.props.ings}
-                 cancelCheckout={this.checkoutCancelHandler}
-                 continueCheckout={this.checkoutContdHandler} />
-                 <Route path={this.props.match.path + '/contact-data'} 
-                //  render= {(props)=>(<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)}
-                component={ContactData} 
-                 />
+                {summary}
+                
             </div>
         )
     }
@@ -52,8 +61,8 @@ class Checkout extends Component{
 
 const mapStateToProps= state =>{
     return {
-        ings: state.ingredients,
-        price: state.totalPrice
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice
     }
 }
 
